566bba2489ad0c8377e1e91b620271a0f0cd5d26
Jahir JF. Figueroa - Ext <jahir.figueroa@GrupoDifare.com>
2026-06-24 10:10:54 -0500

    cambios en video deteccion en vivo 1

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -13,4 +13,13 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-application = get_asgi_application()
+# Inicializar Django ANTES de importar Channels
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from apps.analysis.api.live.routing import websocket_urlpatterns
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": URLRouter(websocket_urlpatterns),
+})
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -93,7 +93,13 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'core.wsgi.application'
+ASGI_APPLICATION = 'core.asgi.application'
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
